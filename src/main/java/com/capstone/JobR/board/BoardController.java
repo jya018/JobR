55d2583a7a25ea140565aa7ad3bae43020b39dea
gksquddnr200 <57342158+gksquddnr200@users.noreply.github.com>
2021-05-19 19:57:32 +0900

    update_5
    plus boardsort

--- src/main/java/com/capstone/JobR/board/BoardController.java
+++ src/main/java/com/capstone/JobR/board/BoardController.java
@@ -68,7 +68,7 @@ public class BoardController {
 		return service.getSearchList(option,keywords);
 	  
 	}
-	/*
+	
 	//각 카테고리내에서 검색
 	@GetMapping ("/search/{boardSort}/{option}/{keywords}")
 	List<BoardVO> boardSortcontent(@PathVariable("boardSort") String boardSort,@PathVariable("option") String option, @PathVariable("keywords") String keywords) 
@@ -77,5 +77,5 @@ public class BoardController {
 		return service.getboardSearchList(boardSort,option,keywords);
 	  
 	}
-	*/
+	
 }
